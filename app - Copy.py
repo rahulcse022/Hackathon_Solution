de5212d18a1aa2673d1d3c217345e46e9a24f580
Rahul Saini <53773271+rahulcse022@users.noreply.github.com>
2021-04-20 12:53:10 +0530

    Add files via upload

---
+++ app - Copy.py
@@ -0,0 +1,96 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+
+## For index page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+## For Breast Cancer page
+@app.route('/breast_cancer/', methods=['POST','GET'])
+def index():
+  return render_template('breast_cancer.html')
+
+
+## For Prediction Page on Breast Cancer
+@app.route('/predict/',methods=['POST'])
+def predict():
+    model = pickle.load(open('model.pkl', 'rb'))
+    input_features = [float(x) for x in request.form.values()]
+    features_value = [np.array(input_features)]
+    
+    features_name = ['mean radius', 'mean texture', 'mean perimeter', 'mean area',
+       'mean smoothness', 'mean compactness', 'mean concavity',
+       'mean concave points', 'mean symmetry', 'mean fractal dimension',
+       'radius error', 'texture error', 'perimeter error', 'area error',
+       'smoothness error', 'compactness error', 'concavity error',
+       'concave points error', 'symmetry error', 'fractal dimension error',
+       'worst radius', 'worst texture', 'worst perimeter', 'worst area',
+       'worst smoothness', 'worst compactness', 'worst concavity',
+       'worst concave points', 'worst symmetry', 'worst fractal dimension']
+    
+    df = pd.DataFrame(features_value, columns=features_name)
+    output = model.predict(df)
+        
+    if output == 0:
+        res_val = "** breast cancer **"
+    else:
+        res_val = "no breast cancer"
+    return render_template('breast_cancer.html', prediction_text='Patient has {}'.format(res_val))
+
+
+
+@app.route('/malaria/', methods=['POST','GET'])
+def malaria():
+  return render_template('malaria.html')
+
+
+## For Prediction Page on Malaria prediction
+@app.route('/predict_malaria/',methods=['POST'])
+def predict_malaria():
+  model = pickle.load(open('malaria.pickle', 'rb'))
+  input_features = [float(x) for x in request.form.values()]
+  features_value = [np.array(input_features)]
+  features_name = ['area_0', 'area_1', 'area_2', 'area_3','area_4']
+  df = pd.DataFrame(features_value, columns=features_name)
+  output = model.predict(df)
+        
+  if output == 'Parasitized':
+    res_val = "** Parasitized **"
+  else:
+    res_val = "Uninfected"
+  return render_template('malaria.html', prediction_text='Patient has {}'.format(res_val))
+
+####### For Heart Disease
+@app.route('/diabetes/', methods=['POST','GET'])
+def diabetes():
+  return render_template('diabetes.html')
+
+@app.route('/predict_diabetes/',methods=['POST'])
+def predict_diabetes():
+  model_diabetes = pickle.load(open('diabetes.pickle', 'rb'))
+  input_features = [float(x) for x in request.form.values()]
+  features_value = [np.array(input_features)]
+  features_name = ["num_preg","glucose_conc","diastolic_bp","thickness","insulin","bmi","diab_pred","age"]      
+
+  df = pd.DataFrame(features_value, columns=features_name)
+  output = model_diabetes.predict(df)
+        
+  if output == 0:
+    res_val = "** Parasitized **"
+  else:
+    res_val = "Uninfected"
+  return render_template('diabetes.html', prediction_text='Patient has {}'.format(res_val))  
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
